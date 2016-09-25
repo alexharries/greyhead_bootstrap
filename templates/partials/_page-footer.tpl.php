05c0dddd8deeafc99a31ebd12495964b98b627fe
Alex Harries <alex@greyhead.co.uk>
2016-09-25 22:15:54 +0200

    Move secondary menu into the page footer (for now).
    Signed-off-by: Alex Harries <alex@greyhead.co.uk>

--- templates/partials/_page-footer.tpl.php
+++ templates/partials/_page-footer.tpl.php
@@ -11,15 +11,31 @@
     </footer>
   </div>
 <?php endif ?>
-<?php if (($footer = render($page['footer'])) || isset($logo)): ?>
-  <div class="footer-wrapper">
-    <footer class="footer container">
-      <?php print $footer ?>
-      <?php if (isset($logo) && !empty($logo)): ?>
-        <a class="logo navbar-btn pull-right" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-          <img src="<?php print $logo; ?>" class="logo" alt="<?php print t('Home'); ?>"/>
-        </a>
-      <?php endif ?>
-    </footer>
-  </div>
-<?php endif ?>
+
+<div class="footer-wrapper">
+  <footer class="footer">
+
+    <!-- Secondary menu, if populated -->
+    <?php if (($secondary_nav = render($secondary_nav)) || ($secondary_navigation = render($page['secondary_navigation']))): ?>
+      <div class="row">
+        <div class="container">
+          <?php print isset($secondary_nav) ? $secondary_nav : NULL; ?>
+
+          <?php print isset($secondary_navigation) ? $secondary_navigation : NULL; ?>
+        </div>
+      </div>
+    <?php endif; ?>
+
+    <div class="row">
+      <div class="container">
+        <?php print render($page['footer']) ?>
+
+        <?php if (isset($logo) && !empty($logo)): ?>
+          <a class="logo navbar-btn pull-right" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+            <img src="<?php print $logo; ?>" class="logo" alt="<?php print t('Home'); ?>"/>
+          </a>
+        <?php endif ?>
+      </div>
+    </div>
+  </footer>
+</div>
